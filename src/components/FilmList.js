70735e6de4190b5ec49bfe761c9fe355f10d2523
Catrin <catrinhughes92@gmail.com>
2020-01-27 15:45:53 +0000

    mvp done

---
+++ src/components/FilmList.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Film from './Film';
+
+class FilmList extends Component {
+
+    render() {
+
+        const filmNodes = this.props.data.map(film => {
+            return (
+                <Film url={film.url} key={film.id} name={film.name}></Film>
+            )
+        })
+
+        return(
+            <div className="film-list">
+                {filmNodes}
+            </div>
+        )
+    }
+
+}
+
+export default FilmList;
